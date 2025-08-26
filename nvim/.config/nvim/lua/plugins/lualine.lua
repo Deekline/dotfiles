e95b26d1d6969a8fb633e731070a631422db27f8
Deekline <nicholas.yemets@gmail.com>
2025-08-26 10:53:17 +0200

    Remove buffers from lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -21,29 +21,6 @@ local config = function()
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = {
-				{
-					"buffers",
-					show_filename_only = true,
-					hide_filename_extension = false,
-					show_modified_status = true,
-					mode = 0,
-					max_length = vim.o.columns * 2 / 3,
-					filetype_names = {
-						TelescopePrompt = "Telescope",
-						dashboard = "Dashboard",
-						packer = "Packer",
-						fzf = "FZF",
-						alpha = "Alpha",
-					},
-					use_mode_colors = false,
-					symbols = {
-						modified = " ●", -- Text to show when the buffer is modified
-						alternate_file = "#", -- Text to show to identify the alternate file
-						directory = "", -- Text to show when the buffer is a directory
-					},
-				},
-			},
 			lualine_x = { "encoding", "fileformat", "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
