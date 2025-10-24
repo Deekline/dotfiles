0d92ebe6f7882163c4e8f5d349886d1adea36a69
Deekline <nicholas.yemets@gmail.com>
2025-10-24 14:43:13 +0200

    Setup blinkcmp

--- nvim/.config/nvim/lua/plugins/blink_cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink_cmp.lua
@@ -1,78 +1,63 @@
+local function link(name, target)
+	vim.api.nvim_set_hl(0, name, { link = target })
+end
+
 return {
 	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
 	dependencies = { "rafamadriz/friendly-snippets" },
-
-	-- use a release tag to download pre-built binaries
 	version = "1.*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
-
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = "super-tab" },
-
-		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
+		keymap = {
+			preset = "super-tab",
+			["<C-n>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback" },
 		},
 
-		-- (Default) Only show the documentation popup when manually triggered
+		appearance = {
+			nerd_font_variant = "mono",
+			use_nvim_cmp_as_default = true,
+		},
+
+		signature = { enabled = true },
+
 		completion = {
-			documentation = {
-				auto_show = false,
-			},
+			documentation = { auto_show = true },
 			ghost_text = {
-				enabled = true,
+				enabled = false,
+				show_with_menu = false,
 			},
 			menu = {
 				border = "rounded",
+				auto_show = true,
 				draw = {
+					treesitter = { "lsp" },
 					columns = {
 						{ "label", "label_description", gap = 1 },
-						{ "kind_icon", "kind" },
+						{ "kind_icon", "kind", "source_name" },
 					},
-					componenets = {
-						label = {
-							kind_icon = {
-								highlight = function(ctx)
-									return { { group = ctx.kind_hl, priority = 20000 } }
-								end,
-							},
+					components = {
+						kind_icon = {
+							highlight = function(ctx)
+								return { { group = ctx.kind_hl, priority = 2001 } }
+							end,
 						},
 					},
 				},
 			},
 		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
+	config = function(_, opts)
+		require("blink.cmp").setup(opts)
+
+		link("BlinkCmpMenuSelection", "CursorLine") -- selected item
+		vim.api.nvim_create_autocmd("ColorScheme", { callback = ensure_selection_hl })
+	end,
+
 	opts_extend = { "sources.default" },
 }
