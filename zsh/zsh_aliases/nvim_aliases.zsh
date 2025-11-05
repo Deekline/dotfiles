5beae58f7adbd31e3a6bc647977ec6adb7045054
Deekline <nicholas.yemets@gmail.com>
2025-11-05 11:35:08 +0100

    Refatoring, add check function and alias for it

--- zsh/zsh_aliases/nvim_aliases.zsh
+++ zsh/zsh_aliases/nvim_aliases.zsh
@@ -33,22 +33,45 @@ use_old_nvim_lint_conform() {
 use_new_nvim_lint_conform() {
   local dir="$NVIM_PLUGINS_DIR"
 
-  if [[ -f "$dir/nvim-lint.lua.bak" && -f "$dir/nvim-lint_old.lua" ]]; then
+  if [[ "$dir/nvim-lint.lua.bak" ]]; then
     mv "$dir/nvim-lint.lua.bak" "$dir/nvim-lint.lua"
-    mv "$dir/nvim-lint_old.lua" "$dir/nvim-lint_old.lua.bak"
   else
     echo "Missing nvim-lint files in $dir" >&2
   fi
 
-  if [[ -f "$dir/conform.lua.bak" && -f "$dir/conform_old.lua" ]]; then
+  if [[ "$dir/nvim-lint_old.lua" ]]; then
+    mv "$dir/nvim-lint_old.lua" "$dir/nvim-lint_old.lua.bak"
+  else
+    echo "Missing nvim-lint_old files in $dir" >&2
+  fi
+
+  if [[ "$dir/conform.lua.bak" ]]; then
     mv "$dir/conform.lua.bak" "$dir/conform.lua"
-    mv "$dir/conform_old.lua" "$dir/conform_old.lua.bak"
   else
     echo "Missing conform files in $dir" >&2
+  fi
+
+  if [[ "$dir/conform_old.lua" ]]; then
+    mv "$dir/conform_old.lua" "$dir/conform_old.lua.bak"
+  else
+    echo "Missing conform_old files in $dir" >&2
   fi
 
   echo "Switched to NEW nvim-lint & conform configs"
 }
 
+check_nvim_config() {
+  local dir="$NVIM_PLUGINS_DIR"
+
+  if [[ -f "$dir/nvim-lint_old.lua.bak" && -f "$dir/conform_old.lua.bak" ]]; then
+    echo "NUXT JS config is enabled" >&2
+  fi
+
+  if [[ -f "$dir/nvim-lint.lua.bak" && -f "$dir/conform.lua.bak" ]]; then
+    echo "Vue JS config is enabled" >&2
+  fi
+}
+
 alias anv='use_old_nvim_lint_conform'
 alias ann='use_new_nvim_lint_conform'
+alias anc='check_nvim_config'
