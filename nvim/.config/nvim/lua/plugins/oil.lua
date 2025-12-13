1da131d23349494348eb4dee8770c1a6fe25c897
Deekline <nicholas.yemets@gmail.com>
2025-12-13 16:47:41 +0100

    Update Oil nvim

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -4,19 +4,37 @@ return {
 		view_options = {
 			show_hidden = true,
 		},
+		float = {
+			border = "rounded", -- "single" | "double" | "rounded" | "solid" | "shadow" | or custom chars
+			win_options = {
+				winbar = "󰉋 %{v:lua.require('oil').get_current_dir()}",
+			},
+			preview_split = "right",
+		},
+
 		preview_win = {
 			-- Whether the preview window is automatically updated when the cursor is moved
 			update_on_cursor_moved = true,
 			-- How to open the preview window "load"|"scratch"|"fast_scratch"
 			preview_method = "fast_scratch",
 			-- A function that returns true to disable preview on a file e.g. to avoid lag
-			disable_preview = function(filename)
+			disable_preview = function()
 				return false
 			end,
 			-- Window-local options to use for preview window buffers
 			win_options = {},
 		},
 	},
+	config = function(_, opts)
+		require("oil").setup(opts)
+
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "OilEnter",
+			callback = function()
+				require("oil").open_preview()
+			end,
+		})
+	end,
 	dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } }, -- use if you prefer nvim-web-devicons
 	lazy = false,
 }
